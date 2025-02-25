c77eb8603067ad94584395e8a82a177bf9f83c05
majd-badran-op <majd.badran@sociumtech.com>
2025-02-25 21:55:37 +0300

    add MethodView

--- main.py
+++ main.py
@@ -1,16 +1,17 @@
 from flask import Flask, request, jsonify, abort
+from flask.views import MethodView
 
 app = Flask(__name__)
 students = []
 id = 1
 
 
-class StudentView:
+class StudentView(MethodView):
     @app.route('/students/create_student', methods=['POST'])
-    def create_student(self):
+    def create_student():
         global id
         data = request.get_json()
-        if len(data.keys()) == 4:
+        if len(data.keys()) == 3:
             student = {
                 'id': id,
                 'name': data['name'],
@@ -19,13 +20,13 @@ class StudentView:
             }
             students.append(student)
             id += 1
-            return jsonify(student, message='a new student was added')
+            return jsonify(student=student, message='a new student was added')
         else:
             abort(400, description='required key missing')
 
     @app.route('/students', methods=['GET'])
     def get_students(self):
-        return jsonify(students)
+        return jsonify(students=students)
 
     @app.route('/students/<int:id>', methods=['GET'])
     def get_student(self, id):
@@ -36,7 +37,7 @@ class StudentView:
                 break
         if student is None:
             abort(404, description='student not found')
-        return jsonify(students)
+        return jsonify(student=student)
 
     @app.route('/students/update_student/<int:id>', methods=['PUT'])
     def update_student(self, id):
@@ -52,9 +53,9 @@ class StudentView:
         student['name'] = data['name']
         student['age'] = data['age']
         student['grade'] = data['grade']
-        return jsonify(students)
+        return jsonify(student=students)
 
-    @app.route('/students/<int:id>', methods=['DELETE'])
+    @app.route('/students/delete_student/<int:id>', methods=['DELETE'])
     def delete_student(self, id):
         student = None
         for s in students:
@@ -66,3 +67,8 @@ class StudentView:
 
         students.remove(student)
         return jsonify(message=f'student with id {id} was deleted')
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
+# curl -X POST http://127.0.0.1:5000/students/create_student -H "Content-Type: application/json" -d '{"name": "John Doe", "age": 20, "grade": "A"}'
