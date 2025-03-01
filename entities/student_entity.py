2ec0c5b443bcdb9231ce94b4b8b6a8ce8e02b056
majd-badran-op <majd.badran@sociumtech.com>
2025-03-01 15:03:10 +0300

    done

--- entities/student_entity.py
+++ entities/student_entity.py
@@ -3,7 +3,7 @@ from entities.baseclass import BaseEntity
 
 class student(BaseEntity):
     def __init__(self, id: int, name: str, age: int, grade: str):
+        super().__init__(id)
         self.name = name
         self.age = age
         self.grade = grade
-        super().__init__(id)
