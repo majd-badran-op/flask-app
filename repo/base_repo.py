fe47ed38821d4a0b853201d2038bd1d8779063c3
majd-badran-op <majd.badran@sociumtech.com>
2025-03-02 10:54:45 +0300

    done

--- repo/base_repo.py
+++ repo/base_repo.py
@@ -1,47 +1,37 @@
-from typing import Generic, TypeVar, Type, Any
+from typing import Generic, TypeVar, Type
 from entities.baseclass import BaseEntity
 
-E = TypeVar('E', bound='BaseEntity')
+E = TypeVar('E', bound=BaseEntity)
 
 
 class BaseRepo(Generic[E]):
-    students: list[dict[str, E]] = []
+    students: dict[str, E] = {}
 
     def __init__(self, entity: Type[E]) -> None:
         self.entity = entity
 
     @classmethod
     def insert(cls, entity: E) -> E:
-        new_student: dict[str, E] = {str(entity.id): entity}
-        cls.students.append(new_student)
+        id_str = str(entity.id)
+        cls.students[id_str] = entity
         return entity
 
     @classmethod
     def get_all(cls) -> list[E]:
-        return [list(student.values())[0] for student in cls.students]
+        return list(cls.students.values())
 
     @classmethod
-    def get(cls, id: int) -> E | None | Any:
-        id_str = str(id)
-        for student_dict in cls.students:
-            if id_str in student_dict:
-                return student_dict[id_str]
-        return None
+    def get(cls, id: int) -> E | None:
+        return cls.students.get(str(id))
 
     @classmethod
     def update(cls, entity: E, id: int) -> bool:
         id_str = str(id)
-        for i, student_dict in enumerate(cls.students):
-            if id_str in student_dict:
-                cls.students[i] = {id_str: entity}
-                return True
+        if id_str in cls.students:
+            cls.students[id_str] = entity
+            return True
         return False
 
     @classmethod
     def delete(cls, id: int) -> bool:
-        id_str = str(id)
-        for i, student_dict in enumerate(cls.students):
-            if id_str in student_dict:
-                cls.students.pop(i)
-                return True
-        return False
+        return cls.students.pop(str(id), None) is not None
